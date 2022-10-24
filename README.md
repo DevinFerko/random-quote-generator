5789ce8cf97eb38a1d04b37e6980493753244bad
DKF-92 <devin.ferko@outlook.com>
2022-10-24 13:33:30 +0100

    Moving project from codepen.io to VSCode/GitHub

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # random-quote-generator
+
+Project that generates a random inspirational quote
+2 buttons: one to generate quote, one to open quote in twitter 
---
+++ app.js
@@ -0,0 +1,73 @@
+const projectName = 'random-quote-machine';
+
+var colors = [
+  '#4dc3ff',
+  '#ffff4d',
+  '#66ff66',
+  '#cc66ff',
+  '#ff9966',
+  '#ff6699',
+  '#6666ff',
+  '#00cc99'
+];
+
+var Quotes = [
+  ["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
+  ["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
+  ["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
+  ["Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.", "Robert Frost"],
+  ["I attribute my success to this: I never gave or took any excuse.", "Florence Nightingale"],
+  ["You miss 100% of the shots you don’t take.", "Wayne Gretzky"],
+  ["An unexamined life is not worth living.", "Socrates"],
+  ["Whatever you can do, or dream you can, begin it.  Boldness has genius, power and magic in it.", "Johann Wolfgang von Goethe"],
+  ["Ask and it will be given to you; search, and you will find; knock and the door will be opened for you.", "Jesus"],
+  ["If you can dream it, you can achieve it.", "Zig Ziglar"]
+];
+
+var currentQuote = "";
+var currentAuthor = "";
+var currentColor= '';
+var randomQuote = "";
+var randomColor = "";
+
+function getQuote (){
+  // Random number functions
+  randomQuote = Math.floor(Math.random() * Quotes.length);
+  randomColor = Math.floor(Math.random() * colors.length);
+  
+  // Assigns Values for quotes, authors, and color
+  currentQuote = Quotes[randomQuote][0];
+  currentAuthor = Quotes[randomQuote][1];
+  currentColor = colors[randomColor];
+  
+  $(document).ready(function () {
+  
+   // Changes background color when called 
+   document.body.style.background = currentColor;
+    
+    // Changes quote when called
+    $('#text').animate({opacity: 0}, 500, function(){
+      $(this).animate({
+        opacity: 1
+      }, 500);
+      $(this).text(currentQuote);
+    });
+    $('#author').animate({opacity:0}, 500, function(){
+      $(this).animate({opacity: 1}, 500);
+      $(this).text('- ' + currentAuthor);
+    });
+  console.log(currentColor);       
+  })
+};
+
+getQuote();
+
+// Calls getQuote function 'on click'
+$(document).ready(function() {
+  $('#new-quote').on('click', getQuote);
+});
+
+
+$(document).ready(function() {
+  $('#twitter').on('click', 'https://twitter.com/intent/tweet/?text=')
+});
